26383761f5eed841c88f42155a5128a0a33c5337
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2025-09-17 22:20:04 +0900

    Example update

--- src/main/java/me/hysong/libcodablejdbc/dev_example/User.java
+++ src/main/java/me/hysong/libcodablejdbc/dev_example/User.java
@@ -10,10 +10,18 @@ import me.hysong.libcodablejdbc.Record;
 import me.hysong.libcodablejdbc.utils.objects.DatabaseElement;
 
 @Getter
-@Record // This applies @Column at all fields with its own name. Name mapping will prioritize @Column(mapTo=xxx).
+@Record // This applies @Column at all fields with its name. Name mapping will prioritize @Column(mapTo=xxx).
 @Database(db="unitable", table="users")
 @PrimaryKey(column="id")
 public class User extends DatabaseElement implements RSCodable {
+
+    /*
+    Database example
+    - unique_id: Integer, PK, AUTO_INCREMENT (Manually mapped to id field using @Column annotation)
+    - name: Text                             (Automatically mapped)
+    - email: Text                            (Automatically mapped)
+    - age: Integer                           (Automatically mapped)
+     */
 
     @Column(mapTo = "unique_id") private final int id = -1;
     @Setter private String name = "John Appleseed";
@@ -27,10 +35,10 @@ public class User extends DatabaseElement implements RSCodable {
     public static void main(String[] args) throws Exception{
         User o = new User();
         o.setPrimaryKeyValue(1);
-        o.select();
+        o.select();                      // Load from database
         System.out.println(o.getName()); // Name is loaded from database
         o.setName("Jonathan Appleseed");
-        o.update(); // Update committed to database
+        o.update();                      // Update committed to database
     }
 }
 
